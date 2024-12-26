da9b8dcc2fda5ef90d01b4c773f1bd2a84b3a3cf
joshua sack <joshsack99@gmail.com>
2024-12-26 16:09:58 -0500

    hell yeah

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 
 import { api } from "~/utils/api";
 
@@ -7,9 +9,9 @@ import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <ClerkProvider>
+    <Component {...pageProps} />
+    </ClerkProvider>
   );
 };
 
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,23 +1,10 @@
-import { Client } from "@planetscale/database";
 import { PrismaClient } from "@prisma/client";
-
 import { env } from "~/env";
-
-const psClient = new Client({ url: env.DATABASE_URL });
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  }).$extends({
-    query: {
-      async $allOperations<T>({ args, query }: { 
-        args: unknown; 
-        query: (args: unknown) => Promise<T>;
-      }): Promise<T> {
-        return psClient.transaction(async () => query(args));
-      },
-    },
   });
 
 const globalForPrisma = globalThis as unknown as {
